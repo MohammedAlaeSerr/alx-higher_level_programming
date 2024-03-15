cb1f10abe7c5c0d578f95005209690c92a413365
Mohamed Alae <serroukhealae@gmail.com>
2024-03-14 19:04:39 -0700

    PYTHON TASKS

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """imports the variable a from the file variable_load_5.py and prints its value"""
-    import variable_load_5
-    print(variable_load_5.a)
+    from variable_load_5 import a
+    print(a)
